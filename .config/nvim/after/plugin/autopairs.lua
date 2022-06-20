12e7e58c4a9c0fa1e4cb6c6b124a133026ea77da
Jan Buchar <Teyras@gmail.com>
2022-06-20 10:56:14 +0200

    Fix autopairs setup

--- .config/nvim/after/plugin/autopairs.lua
+++ .config/nvim/after/plugin/autopairs.lua
@@ -1,7 +1,15 @@
 local autotag = require("nvim-ts-autotag")
 autotag.setup()
 
--- https://github.com/ms-jpq/coq_nvim/issues/91
 local remap = vim.api.nvim_set_keymap
 local npairs = require("nvim-autopairs")
 
+npairs.setup({})
+
+_G.MUtils = {}
+
+MUtils.completion_confirm = function()
+  return npairs.autopairs_cr()
+end
+
+remap("i", "<CR>", "v:lua.MUtils.completion_confirm()", {expr = true, noremap = true})
