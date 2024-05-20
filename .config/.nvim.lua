0325d3c7d49bfe3cada2e19baea61aa3e091625c
Jan Buchar <Teyras@gmail.com>
2024-05-20 22:24:33 +0200

    Bindings for yadm files

---
+++ .config/.nvim.lua
@@ -0,0 +1,34 @@
+local fzf = require("fzf-lua")
+local map = vim.keymap.set
+
+local git_dirs = {
+  git_dir = "~/.local/share/yadm/repo.git",
+  git_worktree = "~"
+}
+
+map(
+  "n",
+  "<leader>m",
+  function()
+    fzf.git_status(vim.tbl_deep_extend("keep", {cmd = "git status -s -uno"}, git_dirs))
+  end,
+  {silent = true}
+)
+
+map(
+  "n",
+  "<leader>n",
+  function()
+    fzf.git_files(vim.tbl_deep_extend("keep", {cwd = vim.loop.cwd()}, git_dirs))
+  end,
+  {silent = true}
+)
+
+map(
+  "n",
+  "<leader>l",
+  function()
+    fzf.git_commits(git_dirs)
+  end,
+  {silent = true}
+)
