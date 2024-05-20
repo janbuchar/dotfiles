c57f74d9b0b5c211759f02e58ed7598c9f79c1b9
Jan Buchar <Teyras@gmail.com>
2024-05-20 11:22:26 +0200

    Disable unnecessary providers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,6 +17,12 @@ vim.opt.rtp:prepend(lazypath)
 -- Configure leader key
 vim.g.mapleader = " "
 
+-- Disable unnecessary providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 -- Set up plugins
 require("lazy").setup(
   "plugins",
