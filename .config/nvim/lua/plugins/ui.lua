8f4eb8c69cf32d82df8e6e3c949f2efa3da6693d
Jan Buchar <Teyras@gmail.com>
2024-05-30 21:12:30 +0200

    yazi pimpage

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -45,7 +45,8 @@ return {
     ---@type YaziConfig
     opts = {
       open_for_directories = false,
-      floating_window_scaling_factor = 0.75
+      floating_window_scaling_factor = 0.75,
+      yazi_floating_window_winblend = 10
     }
   },
   {
