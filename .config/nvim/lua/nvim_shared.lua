2c3de44a544b4bb9c33d7e9a7e1735812c6a8a53
Jan Buchar <Teyras@gmail.com>
2024-05-30 21:13:26 +0200

    add biome formatting preset

--- .config/nvim/lua/nvim_shared.lua
+++ .config/nvim/lua/nvim_shared.lua
@@ -17,6 +17,18 @@ M.formatters.prettier = function()
   }
 end
 
+M.formatters.biome = function()
+  return {
+    exe = "biome",
+    args = {
+      "format",
+      "--stdin-file-path",
+      vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
+    },
+    stdin = true
+  }
+end
+
 M.formatters.eslint = function()
   return {
     exe = "eslint",
