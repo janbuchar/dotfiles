695bbad520dff806750aca876c1fcf2bcfc79006
Jan Buchar <Teyras@gmail.com>
2024-05-30 21:13:58 +0200

    Add tmux clipboard integration

---
+++ .config/nvim/after/plugin/clipboard.lua
@@ -0,0 +1,16 @@
+(function()
+  if not vim.env.TMUX then
+    return
+  end
+
+  vim.api.nvim_create_autocmd(
+    {"TextYankPost"},
+    {
+      group = vim.api.nvim_create_augroup("tmux_clipboard", {clear = true}),
+      pattern = "*",
+      callback = function()
+        vim.system({"tmux", "loadb", "-w", "-"}, {stdin = table.concat(vim.v.event.regcontents, "\n")})
+      end
+    }
+  )
+end)()
