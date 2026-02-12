90819da58278310bfcb11ec5fc29441f817bc233
Jan Buchar <Teyras@gmail.com>
2026-02-12 13:21:08 +0100

    feat(lsp): add dev-tools.nvim with "Open in browser" code action
    Adds a code action that opens the current line/selection in the
    browser using Snacks.gitbrowse.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -34,4 +34,29 @@ return {
       enabled = true,
     },
   },
+  {
+    "yarospace/dev-tools.nvim",
+    opts = {
+      actions = {
+        {
+          name = "Open in browser",
+          fn = function(action)
+            local ctx = action.ctx
+
+            local line_start = ctx.row + 1
+            local line_end = ctx.row + 1
+
+            if ctx.range ~= nil then
+              line_end = ctx.range["end"].line + 1
+            end
+
+            Snacks.gitbrowse.open({
+              line_start = line_start,
+              line_end = line_end,
+            })
+          end,
+        },
+      },
+    },
+  },
 }
