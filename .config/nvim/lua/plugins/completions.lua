6cd0d311cdedca13c4d90a90bc50f738a84ea4ae
Jan Buchar <Teyras@gmail.com>
2026-04-09 18:04:06 +0200

    Patch up blink.cmp config

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -4,7 +4,7 @@ return {
     version = "1.*",
     dependencies = {
       "moyiz/blink-emoji.nvim",
-      "barrettruth/blink-cmp-tmux",
+      "mgalliou/blink-cmp-tmux",
     },
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
@@ -13,11 +13,13 @@ return {
         preset = "none",
         ["<C-b>"] = { "scroll_documentation_up", "fallback" },
         ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-        ["<C-Space>"] = { "show", "fallback" },
+        ["<C-Space>"] = { "select_and_accept", "fallback" },
         ["<C-e>"] = { "cancel", "fallback" },
-        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "insert_next", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "insert_prev", "snippet_backward", "fallback" },
+        ["<Down>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<Up>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<CR>"] = { "hide", "fallback" },
       },
       appearance = {
         nerd_font_variant = "mono",
@@ -41,12 +43,26 @@ return {
         documentation = {
           auto_show = true,
         },
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
+        },
       },
       signature = {
         enabled = true,
       },
       cmdline = {
         enabled = true,
+        keymap = {
+          preset = "inherit",
+          ["<CR>"] = { "fallback" },
+          ["<C-Space>"] = { "select_accept_and_enter", "fallback" },
+        },
+        completion = {
+          menu = { auto_show = true },
+        },
       },
     },
   },
