3bd778483a7f3b3a936080a1b4bd34a420667616
Jan Buchar <Teyras@gmail.com>
2026-04-26 00:51:19 +0200

    Ignore obscure treesitter grammars

--- .config/nvim/lua/plugins/syntax.lua
+++ .config/nvim/lua/plugins/syntax.lua
@@ -5,6 +5,16 @@ return {
     main = "nvim-treesitter.configs",
     opts = {
       ensure_installed = "all",
+      -- nvim-treesitter master branch is archived as of 2026-04-03.
+      -- These grammars fail to compile (tree-sitter CLI --no-bindings incompatibility):
+      --   ipkg, latex, mlir, ocamllex, scfg, swift, teal, unison
+      -- These use the built-in nvim 0.12 parser instead (plugin's injection queries
+      -- use the defunct #set-lang-from-info-string! directive, causing nil node crashes):
+      --   markdown, markdown_inline
+      ignore_install = {
+        "ipkg", "latex", "mlir", "ocamllex", "scfg", "swift", "teal", "unison",
+        "markdown", "markdown_inline",
+      },
       highlight = {
         enable = true,
       },
